838f75c5c6aaf410714e4318c6acef91662679f9
bevieboo <chardos@gmail.com>
2016-03-20 13:40:23 +1100

    Added more methods to API

--- app/controllers/api/monkeys_controller.rb
+++ app/controllers/api/monkeys_controller.rb
@@ -1,5 +1,43 @@
 class Api::MonkeysController <  ApplicationController
+  skip_before_filter :verify_authenticity_token
   def index
     render json: Monkey.all
   end
+
+  def show
+    monkey = Monkey.find(params[:id])
+    render json: monkey
+  end
+
+  def create
+    monkey = Monkey.new(monkey_params)
+    if monkey.save
+      render json: {
+        status: 200,
+        message: "Successfully created a monkey",
+        monkey: monkey
+      }.to_json
+    else
+      render json: {
+        status: 500,
+        errors: monkey.errors
+      }.to_json
+    end
+  end
+
+  def destroy
+    monkey = Monkey.find(params[:id])
+    monkey.destroy
+    render json:{
+      status: 200,
+      message: 'Successfully deleted monkey'
+    }.to_json
+  end
+
+  private
+
+  def monkey_params
+    params.require('monkey').permit('name')
+  end
+
 end
