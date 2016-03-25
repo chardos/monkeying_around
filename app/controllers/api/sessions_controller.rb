8f0aaff6f881e01c6bce29ef0bb4b8cecca73adc
bevieboo <chardos@gmail.com>
2016-03-25 19:02:54 +1100

    added more stuff

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -16,10 +16,8 @@ class Api::SessionsController < ApplicationController
   end
 
   def destroy
-   user = User.find_by(auth_token: params[:id])
-   user.generate_authentication_token!
-   user.save
-   head 204
+    current_user.destroy
+    head 204
   end
 
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,7 @@
 class Api::UsersController < ApplicationController
+  before_action :authenticate_with_token!, only: [:update, :destroy]
+  respond_to :json
+
   def index
     render json: User.all
   end
@@ -13,6 +16,16 @@ class Api::UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = current_user
+
+    if user.update(user_params)
+      render json: user, status: 200, location: [:api, user]
     else
       render json: { errors: user.errors }, status: 422
     end
