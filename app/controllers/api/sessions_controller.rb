387a0266ec1f0e6aad72d231c5ecb911d10e2192
bevieboo <chardos@gmail.com>
2016-03-25 18:47:14 +1100

    Add authenticable

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -15,4 +15,11 @@ class Api::SessionsController < ApplicationController
     end
   end
 
+  def destroy
+   user = User.find_by(auth_token: params[:id])
+   user.generate_authentication_token!
+   user.save
+   head 204
+  end
+
 end
